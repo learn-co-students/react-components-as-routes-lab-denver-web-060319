3996f2157ba2a18b71b2586a222ab27abab59e25
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-08-21 08:45:52 -0600

    Implement routes and navbar
    Utilize react-router-dom

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorsList = directors.map(director =>
+    <div>
+      <h3>{director.name}</h3>
+      <ul>
+        {director.movies.map(movie =>
+          <li>{movie}</li>
+        )}
+      </ul>
+    </div>
+  )
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsList}
     </div>
   );
 }
